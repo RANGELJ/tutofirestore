dd1ded002b485b94f9ebda6aefb93e8765b5e8f1
Jorge Rangel <jorgerangel@192.168.1.5>
2024-02-11 11:17:13 -0600

    Remove redux lib

--- client/src/hooks/useSelector.ts
+++
@@ -1,8 +0,0 @@
-import { useSelector as useSelectorOriginal } from 'react-redux'
-import { StoreState } from '../shared/storeState'
-
-const useSelector = <T>(
-    selector: (state: StoreState) => T
-): T => useSelectorOriginal(selector)
-
-export default useSelector
